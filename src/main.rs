64a182ba3a6d2479fabf9fba30e34045570b146f
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-10-30 12:59:31 +0300

    adding values to a vector

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,19 @@ fn main() {
     let v0: Vec<i32> = Vec::new();
 
     //creating a vector without specifying the data type
-    let mut v1 = vec![1, 2, 3];
+    let v1 = vec![1, 2, 3];
 
     println!("the first vector is: {:?}", &v0);
 
     println!("our second vector has the following data: {:?}", &v1);
+
+    //updating a vector
+    let mut v2 = vec![1, 2, 3];
+
+    v2.push(4);
+    v2.push(5);
+    v2.push(6);
+
+    println!("the updated vector has values: {:?}", v2);
+
 }
