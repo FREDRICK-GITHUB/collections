34f505e2dc479024bb9116512d3af66ed559d4d4
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-11-02 08:41:20 +0300

    added hashmaps and all the associated operations performed on hashmaps

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,64 @@
+use std::collections::HashMap;
 fn main() {
-    let mut s = String::new();
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"),10);
+    scores.insert(String::from("Yellow"), 50);
 
-    let data = "initial contents";
-    let s1 = data.to_string();
+    let team_name = String::from("Blue");
 
-    let s2 = "initial contents".to_string();
+    //accessing value whose key is supplied
+    let score = scores.get(&team_name).copied().unwrap_or(0);
 
-    println!("we have an empty string: {} ",s );
+    println!("The score for team blue is: {}", score);
 
-    //creating a string from a string literal
-    let s3 = String::from("initial contents");
+    //how to iterate through a Hashmap
+    for (key, value) in &scores {
+        println!("KEY: {}, VALUE: {}", key, value);
+    }
 
-    //appending to a string using push_str
-    let mut s4 = String::from("foo");
-    s4.push_str("bar");
-    println!("our new string is: {}", s4);
+    //ownership in hashmaps
+    let field_name = String::from("Favourite color");
+    let field_value = String::from("Blue");
 
-    //appending to a string using push. Push takes a single character and adds it to a string
-    let mut s5 = String::from("lo");
-    s5.push('l');
-    println!("the string with a new character is: {}", s5);
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
 
-    //concatenation using + operator
-    let s6 = String::from("Hello ");
-    let s7 = String::from("world!");
-    let s8 = s6 + &s7;
-    println!("{}", s8);
+    // println!("The field name: {} and field_value: {} are now owned by the hasmap", field_name, field_value);
 
-    //concatenation using the format! Macro
-    let t1 = String::from("tic");
-    let t2 = String::from("tac");
-    let t3 = String::from("toe");
+    //updating a hashmap
+    //updating by overwriting a value
+    let mut scores1 = HashMap::new();
+    scores1.insert(String::from("Blue"), 10);
+    scores1.insert(String::from("Blue"),33);
 
-    let t = t1 + "-" + &t2 + "-" + &t3;
-    println!("{}", t);
+    let correct_score = String::from("Blue");
+    println!("The value in the hashmap is: {}", scores1.get( &correct_score ).copied().unwrap_or(0));
+
+    //adding a key and a value only if a key does not exist using entry API
+    let mut scores2 = HashMap::new();
+    scores2.insert(String::from("Blue"), 13);
+
+    scores2.entry(String::from("Yellow")).or_insert(25);
+    scores2.entry(String::from("Blue")).or_insert(65);
+
+    let score2_value = String::from("Blue");
+    for (key, value) in &scores2 {
+        println!("The values in hashmap are: key-{} value-{}", key, value);
+    }
 
 
-    let tt1 = String::from("tic");
-    let tt2 = String::from("tac");
-    let tt3 = String::from("toe");
-    let tt4 = format!("{}-{}-{}", tt1, tt2, tt3);
-    println!("{}", tt4);
+    //updating value based on the old value
 
-    /* Indexing into Strings brings in issues especially for UTF-8 
-    this is because a character in a string could be taking more than one byte. 
-    This makes it hard to know where a character starts and where it ends */
+    let text = "hello world beautiful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
+    
 
 
 }
